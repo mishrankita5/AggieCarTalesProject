7b7337f3ec70bdb25b405cae8f07ee310e5ead2a
ankitamishra <mishrankita5@tamu.edu>
2020-04-03 23:46:34 -0500

    routing login

--- app.py
+++ app.py
@@ -21,6 +21,7 @@ class User(db.Model):
     user_id = db.Column(db.Integer,primary_key = True)
     firstName = db.Column(db.String(50))
     lastName = db.Column(db.String(50))
+    # TODO - Make Email Unique
     email = db.Column(db.VARCHAR(length=100))
     password = db.Column(db.VARCHAR(length=20))
     yearOfGraduation = db.Column(db.Integer)
@@ -46,27 +47,27 @@ def routeLogin():
     return render_template('login.html')
 
 @app.route('/about-us.html')
-def about():
+def routeAbout():
     return render_template('about-us.html')
 
 @app.route('/index.html')
-def home():
+def routeHome():
     return render_template('index.html')
 
 @app.route('/ad-listing.html')
-def addListing():
+def routeAddListing():
     return render_template('ad-listing.html')
 
 @app.route('/user-profile.html')
-def userProfile():
+def routeUserProfile():
     return render_template('user-profile.html')
 
 @app.route('/single-blog.html')
-def singleBlog():
+def routeSingleBlog():
     return render_template('single-blog.html')
 
 @app.route('/blog.html')
-def blog():
+def routeBlog():
     return render_template('blog.html')
 
 @app.route('/register', methods = ['POST'])
@@ -87,6 +88,32 @@ def register():
             db.session.commit()
             return render_template ('index.html', message='Welcome to Aggie Car Tales!')
         return render_template('register.html', message='User already exists. Kindly login.')
+
+@app.route('/login' , methods =['POST'])
+def login():
+    if request.method == 'POST':
+        email = request.form['emailId']
+        password = request.form['password']
+        print(password)
+        records = db.session.query(User).filter(User.email == email).all()
+        for record in records:
+            # recordObject = {'id': record.user_id,
+            #                 'user_firstName': record.firstName,
+            #                 'user_lastName': record.lastName,
+            #                 'user_email': record.email,
+            #                 'user_password': record.password,
+            #                 'user_yearOfGraduation': record.yearOfGraduation
+            #                 }
+            dbPassword = record.password
+            # print(record.password)
+            if dbPassword == password:
+               print("password match")
+            print("password don't match")
+
+        
+        # if db.session.query(User).filter(User.email == email).count() > 0:
+    return render_template ('index.html', message='Welcome to Aggie Car Tales!')
+        
 
 if __name__ == '__main__':
     
