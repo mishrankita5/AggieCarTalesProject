c304e50cba5e8e90c85119fb7c6ca59e0ac664c1
ankitamishra <mishrankita5@tamu.edu>
2020-04-24 15:24:16 -0500

    penultimate changes

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ app = Flask(__name__)
 app.secret_key = "randomstring123"
 
 
-ENV = 'dev'
+ENV = 'prod'
 
 if ENV == 'dev':
     app.debug = True
@@ -80,6 +80,29 @@ class Feedback(db.Model):
 
 @app.route('/')
 def index():
+    records = db.session.query(Review).join(User,Review.user_id==User.user_id).add_columns(User.firstName,Review.carName,Review.carModel,Review.carCategory,Review.review,Review.review_id,Review.yearOfManufacturing, Review.carImage, Review.reviewDate, Review.carCategory).order_by(Review.review_id.desc()).limit(4).all()
+    reviewVar = ["" for x in range(len(records))]
+    adminVar = ["" for x in range(len(records))]
+    carNameVar = ["" for x in range(len(records))]
+    carModelVar = ["" for x in range(len(records))]
+    reviewIdVar = ["" for x in range(len(records))]
+    carImageVar = ["" for x in range(len(records))]
+    reviewDateVar = ["" for x in range(len(records))]
+    carCategoryVar = ["" for x in range(len(records))]
+    i=0
+    for record in records:
+        reviewVar[i] = record.review 
+        adminVar[i] = record.firstName
+        carNameVar[i] = record.carName
+        carModelVar[i] = record.carModel
+        reviewIdVar[i] = record.review_id
+        carImageVar[i] = b64encode(record.carImage).decode("utf-8")
+        reviewDateVar[i] = record.reviewDate
+        carCategoryVar[i] =record.carCategory
+
+        i=i+1
+    return render_template('index.html',len = len(records),reviewVar=reviewVar,adminVar=adminVar,carNameVar=carNameVar,carModelVar=carModelVar,reviewIdVar=reviewIdVar, carImageVar = carImageVar, reviewDateVar=reviewDateVar,carCategoryVar=carCategoryVar)
+    #return render_template('index.html')
     return render_template('index.html')
 
 @app.route('/404.html')
@@ -104,6 +127,28 @@ def routeAbout():
 
 @app.route('/index.html')
 def routeHome():
+    records = db.session.query(Review).join(User,Review.user_id==User.user_id).add_columns(User.firstName,Review.carName,Review.carModel,Review.carCategory,Review.review,Review.review_id,Review.yearOfManufacturing, Review.carImage, Review.reviewDate, Review.carCategory).order_by(Review.review_id.desc()).limit(4).all()
+    reviewVar = ["" for x in range(len(records))]
+    adminVar = ["" for x in range(len(records))]
+    carNameVar = ["" for x in range(len(records))]
+    carModelVar = ["" for x in range(len(records))]
+    reviewIdVar = ["" for x in range(len(records))]
+    carImageVar = ["" for x in range(len(records))]
+    reviewDateVar = ["" for x in range(len(records))]
+    carCategoryVar = ["" for x in range(len(records))]
+    i=0
+    for record in records:
+        reviewVar[i] = record.review 
+        adminVar[i] = record.firstName
+        carNameVar[i] = record.carName
+        carModelVar[i] = record.carModel
+        reviewIdVar[i] = record.review_id
+        carImageVar[i] = b64encode(record.carImage).decode("utf-8")
+        reviewDateVar[i] = record.reviewDate
+        carCategoryVar[i] =record.carCategory
+
+        i=i+1
+    return render_template('index.html',len = len(records),reviewVar=reviewVar,adminVar=adminVar,carNameVar=carNameVar,carModelVar=carModelVar,reviewIdVar=reviewIdVar, carImageVar = carImageVar, reviewDateVar=reviewDateVar,carCategoryVar=carCategoryVar)
     return render_template('index.html')
 
 @app.route('/addreview.html')
